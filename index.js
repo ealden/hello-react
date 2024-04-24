3356dcd8fb6b5e381727065d50e19d0bc9cef2cb
Ealden Esto E. Escañan <ealden@escanan.com>
2024-04-24 11:05:28 +0900

    Writing Markup with JSX

--- index.js
+++ index.js
@@ -13,12 +13,22 @@ function CreatingAndNestingComponents() {
   )
 }
 
+function WritingMarkupWithJSX() {
+  return (
+    <>
+      <p>This is an empty wrapper</p>
+    </>
+  )
+}
+
 let App = function MyApp() {
   return (
     <div>
       <h1>Hello, React!</h1>
       <h2>Creating and Nesting Components</h2>
       <CreatingAndNestingComponents />
+      <h2>Writing Markup with JSX</h2>
+      <WritingMarkupWithJSX />
     </div>
   );
 }
