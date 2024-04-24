f9bb65fa39d2cbcfe09cf7312ea0b0110a046763
Ealden Esto E. Escañan <ealden@escanan.com>
2024-04-24 11:01:20 +0900

    Creating and Nesting Components

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  )
+}
+
+function CreatingAndNestingComponents() {
+  return (
+    <MyButton />
+  )
+}
+
 let App = function MyApp() {
   return (
     <div>
       <h1>Hello, React!</h1>
+      <h2>Creating and Nesting Components</h2>
+      <CreatingAndNestingComponents />
     </div>
   );
 }
